7f3153502da975ea21e7c9130ba253ccd175a7eb
Elaine Yeung <yeung.egs@gmail.com>
2017-09-30 00:21:13 +0000

    Task 0 - import UUID for cache_id

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,7 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-
+from uuid import uuid4, UUID
 
 # flask setup
 app = Flask(__name__)
@@ -23,7 +23,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/0-hbnb/')
+@app.route('/0-hbnb')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
@@ -34,11 +34,13 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    return render_template('100-hbnb.html',
+    cache_id = str(uuid4())
+    return render_template('/0-hbnb.html',
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users)
+                           users=users,
+                           cache_id=cache_id)
 
 if __name__ == "__main__":
     """
