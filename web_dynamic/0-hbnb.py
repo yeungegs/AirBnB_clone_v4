e1e66de0219dacd97c16a2f8f6ce56325329d22f
spencerhcheng <spencerhcheng@gmail.com>
2017-09-30 00:52:30 +0000

    Task 1: remove string cast of uuid4 and fix formatting of html template

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -23,7 +23,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/0-hbnb')
+@app.route('/0-hbnb/')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
@@ -34,7 +34,7 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    cache_id = str(uuid4())
+    cache_id = uuid4()
     return render_template('/0-hbnb.html',
                            states=states,
                            amens=amens,
