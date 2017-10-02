5308cce871392bac9536ad7706deced7059e014c
spencerhcheng <spencerhcheng@gmail.com>
2017-10-02 00:49:06 +0000

    Display amenities list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,36 @@
+$(function () {
+  const amenStor = {};
+  $('LI INPUT[type=checkbox').change(function (event) {
+    let amenId = $(this).attr('data-id');
+    let amenName = $(this).attr('data-name');
+    if (this.checked) {
+      amenStor[amenId] = amenName;
+    } else {
+      delete amenStor[amenId];
+    }
+    let amenStr = "";
+    let first_flag = 0;
+    let first_key = "";
+    for (key in amenStor) {
+	if (first_flag === 0) {
+		first_key = key;
+		amenStr += amenStor[key];
+		first_flag = 1;
+	}
+	else if (first_flag === 1) {
+		if (key === first_key) {
+			continue;
+		}
+		amenStr += ", " + amenStor[key];
+	}
+    }
+	if (amenStr.length > 30) {
+		amenStr = amenStr.slice(0, 27);
+		amenStr += "...";
+	}
+	console.log(amenStr);
 
+//    $.each(amenStor, function (key, value) {
+      $('.amenities h4').text(amenStr);
+  });
+});
