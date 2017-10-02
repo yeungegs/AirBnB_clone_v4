72885a0a7650e33e4ffe63e298a32894de4d8663
spencerhcheng <spencerhcheng@gmail.com>
2017-10-02 00:59:49 +0000

    Display amenities selected in H4 tag

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -8,29 +8,25 @@ $(function () {
     } else {
       delete amenStor[amenId];
     }
-    let amenStr = "";
+    let amenStr = '';
     let first_flag = 0;
-    let first_key = "";
+    let first_key = '';
     for (key in amenStor) {
-	if (first_flag === 0) {
-		first_key = key;
-		amenStr += amenStor[key];
-		first_flag = 1;
-	}
-	else if (first_flag === 1) {
-		if (key === first_key) {
-			continue;
-		}
-		amenStr += ", " + amenStor[key];
-	}
+      if (first_flag === 0) {
+        first_key = key;
+        amenStr += amenStor[key];
+        first_flag = 1;
+      }	else if (first_flag === 1) {
+        if (key === first_key) {
+          continue;
+        }
+        amenStr += ', ' + amenStor[key];
+      }
     }
-	if (amenStr.length > 30) {
-		amenStr = amenStr.slice(0, 27);
-		amenStr += "...";
-	}
-	console.log(amenStr);
-
-//    $.each(amenStor, function (key, value) {
-      $('.amenities h4').text(amenStr);
+    if (amenStr.length > 30) {
+      amenStr = amenStr.slice(0, 29);
+      amenStr += '...';
+    }
+    $('.amenities h4').text(amenStr);
   });
 });
